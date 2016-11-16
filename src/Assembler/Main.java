6bd8004ed977e68a33bae229e12733a356562ef3
Quazi Irfan GD <quazirfan@gmail.com>
2016-11-15 20:38:50 -0600

    Added comment

--- src/Assembler/Main.java
+++ src/Assembler/Main.java
@@ -8,17 +8,21 @@ import java.util.LinkedList;
 
 //  NAME  :  Quazi Irfan
 //  CLASS  :  CSc 354
-//  ASSIGNMENT : 3
-//  DUE DATE : 11/4/16
+//  ASSIGNMENT : 4
+//  DUE DATE : 11/16/16
 //  INSTRUCTOR :  Dr. Hamer
-//  DESCRIPTION : Assignment 4
+//  DESCRIPTION : Assignment 4 : Pass 2
 
 /**
  * Assembler.Main is the Entry point of Assignment 4 : Pass 2
  *
- * Input : Intermediate file(file format ending with .int) created from assignment 1
- * If not then the program requests a file name. If file not found, the program terminates.
- * If found, a static method from Pass1Utility class is called. The last parameter of this method is the output file name.
+ * Input : A SICXE assembly source.
+ * Output : The program will generate 3 files.
+ * During Pass 1 : One intermediate file ending in .int with line counter added to the left of every line.
+ * During Pass 2 : One update intermediate file ending in .txt extension with object code added to the right of every instruction.
+ * And one object file ending in .o extension.
+ *
+ * If no file is provides as command line argument, the program will ask for a source file.
  * We process every instruction, and print output of Pass 1 in that file.
  *
  */
@@ -27,9 +31,23 @@ public class Main {
         SymbolTable symbolTable = new SymbolTable();
         LinkedList<Literal> literalTable = new LinkedList<>();
 
+//        // set input files
+//        String inputFile;
+//        if(args.length < 1){
+//            System.out.println("Missing command like argument.");
+//
+//            // request the intput SIC/XE Assembly file
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//            System.out.print("Enter SIC Assembly Source file : ");
+//            inputFile = reader.readLine();
+//        } else {
+//            inputFile = args[0];
+//        }
+
 //        String inputFile = "SICXE Program 4.asm";
 //        String inputFile = "CS_Func.asm";
         String inputFile = "A3_4.asm";
+
         System.out.println("Reading from File : " + inputFile );
 
         // This function creates an intermediate file in .inc extension, and populates symbol and literal table
@@ -79,52 +97,5 @@ public class Main {
         System.out.println("\n> Generated Object Code");
         String ofileName = inputFile.substring(0, inputFile.indexOf('.')).concat(".o");
         Utility.printFile(ofileName);
-
-//        // Populate Symbol Table
-//        System.out.println("*** SYMBOL TABLE ***\nSymbol\t Value\t rflag\t iflag\t mflag\t");
-//        BufferedReader symbolReader = new BufferedReader(new FileReader("OldTestFiles/A2_labels.txt"));
-//        String symbolLine = symbolReader.readLine();
-//        while(symbolLine != null){
-//            symbolTable.addLine(symbolLine);
-//            symbolLine = symbolReader.readLine();
-//        }
-//        symbolTable.view();
-//
-//        // Evaluate and print Expressions
-//        System.out.println("\n*** EXPRESSIONS ***\nExpresion\t\t Value\t Relocatable n i x");
-//        BufferedReader operandReader = new BufferedReader(new FileReader("OldTestFiles/A2_operands.txt"));
-//        String operandLine = operandReader.readLine();
-//        while(operandLine != null) {
-//            OperandUtility.evaluateOperand(symbolTable, literalTable, operandLine);
-//            operandLine = operandReader.readLine();
-//        }
-
-//        Pass1Utility.generateIntermediate("A3_1.asm", symbolTable, literalTable);
-//        System.out.println("************************************************************");
-//        Pass1Utility.generateIntermediate("A3_2.asm", symbolTable, literalTable);
-//        System.out.println("************************************************************");
-//        Pass1Utility.generateIntermediate("A3_3.asm", symbolTable, literalTable);
-//        System.out.println("************************************************************");
-//        Pass1Utility.generateIntermediate("A3_4.asm", symbolTable, literalTable);
-
-
-//        // Print contents of the literal table
-//        System.out.println("\n*** LITERAL TABLE ***\nName\t\t Value\t\t Size\t Address");
-//        for(Object o : literalTable){
-//            System.out.println(o);
-//        }
-
-//        // set input files
-//        String inputFile;
-//        if(args.length < 1){
-//            System.out.println("Missing command like argument.");
-//
-//            // request the intput SIC/XE Assembly file
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//            System.out.print("Enter SIC Assembly Intermediate file : ");
-//            inputFile = reader.readLine();
-//        } else {
-//            inputFile = args[0];
-//        }
     }
 }
